a65df16806a758f2d3137d426ed4f5cf74a74565
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-05-09 12:03:10 +0700

    Memberi Aksi Pada Tombol Saat Diklik

--- app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
+++ app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
@@ -5,8 +5,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -14,9 +19,15 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.satyayudha0077.assessment_mobpro.ui.theme.Assessment_mobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -53,19 +64,35 @@ fun MainScreen() {
 
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello Android",
-        modifier = modifier
-    )
+    var number by remember { mutableIntStateOf(0) }
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = number.toString(),
+            style = MaterialTheme.typography.displayLarge
+        )
+        Button(
+            onClick = { number++ },
+            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
+            contentPadding = PaddingValues(16.dp)
+        ) {
+            Text(
+                text = stringResource(R.string.count)
+            )
+        }
+    }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
