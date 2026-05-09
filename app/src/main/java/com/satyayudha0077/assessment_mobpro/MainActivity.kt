68a82af9b9f41cd474fde5e85441523f52c75d94
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-05-09 12:16:00 +0700

    Menambahkan Data Buah

--- app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
+++ app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.satyayudha0077.assessment_mobpro.model.Buah
 import com.satyayudha0077.assessment_mobpro.ui.theme.Assessment_mobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,6 +46,14 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val data = listOf(
+        Buah("Alpukat", R.drawable.alpukat),
+        Buah("Apel", R.drawable.apel),
+        Buah("Jambu", R.drawable.jambu),
+        Buah("Jeruk", R.drawable.jeruk),
+        Buah("Pisang", R.drawable.pisang),
+    )
+
     Scaffold(
         topBar = {
             TopAppBar(
---
+++ app/src/main/java/com/satyayudha0077/assessment_mobpro/model/Buah.kt
@@ -0,0 +1,6 @@
+package com.satyayudha0077.assessment_mobpro.model
+
+data class Buah(
+    val nama: String,
+    val imageResId: Int
+)
