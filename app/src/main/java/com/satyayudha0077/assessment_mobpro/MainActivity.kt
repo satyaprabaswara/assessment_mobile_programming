49ed64f949184a595e6e0068a216543100a1cc2e
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-05-09 12:35:14 +0700

    Memisahkan Composable dari Activity

--- app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
+++ app/src/main/java/com/satyayudha0077/assessment_mobpro/MainActivity.kt
@@ -1,38 +1,10 @@
 package com.satyayudha0077.assessment_mobpro
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.satyayudha0077.assessment_mobpro.model.Buah
+import com.satyayudha0077.assessment_mobpro.ui.screen.MainScreen
 import com.satyayudha0077.assessment_mobpro.ui.theme.Assessment_mobproTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,98 +16,5 @@ class MainActivity : ComponentActivity() {
                 MainScreen()
             }
         }
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    val data = listOf(
-        Buah("Alpukat", R.drawable.alpukat),
-        Buah("Apel", R.drawable.apel),
-        Buah("Jambu", R.drawable.jambu),
-        Buah("Jeruk", R.drawable.jeruk),
-        Buah("Pisang", R.drawable.pisang),
-    )
-
-    var index by remember { mutableIntStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(id = R.string.app_name))
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
-            )
-        }
-    ) { innerPadding ->
-        ScreenContent(data[index], Modifier.padding(innerPadding)) {
-            index = if (index == data.size-1) 0 else index + 1
-        }
-    }
-}
-
-@Composable
-fun ScreenContent(buah: Buah, modifier: Modifier = Modifier, onClick:() -> Unit) {
-    var number by remember { mutableIntStateOf(0) }
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Column(
-            modifier = modifier.fillMaxSize().padding(16.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Image(
-                painter = painterResource(id = buah.imageResId),
-                contentDescription = stringResource(R.string.gambar, buah.nama),
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(132.dp)
-            )
-            Text(
-                text = buah.nama,
-                style = MaterialTheme.typography.headlineLarge,
-                modifier = modifier.padding(top = 16.dp)
-            )
-            Button(
-                onClick = { onClick() },
-                modifier = Modifier.fillMaxWidth(0.5f).padding(top = 24.dp),
-                contentPadding = PaddingValues(16.dp)
-            ) {
-                Text(text = stringResource(R.string.lanjut))
-            }
-        }
-        Button(
-            onClick = { number++ },
-            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            Text(
-                text = stringResource(R.string.count)
-            )
-        }
-    }
-}
-
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Text(
-//        text = "Hello $name!",
-//        modifier = modifier
-//    )
-//}
-
-@Preview(showBackground = true)
-@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    Assessment_mobproTheme {
-        MainScreen()
     }
 }
