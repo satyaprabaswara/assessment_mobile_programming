02e158fdaabd12809519f55c15fef3c37a300918
Satya Yudha Prabaswara <satya.yprabaswara@users.noreply.github.com>
2026-05-09 14:25:15 +0700

    Memberi pilihan kepada pengguna

--- app/src/main/java/com/satyayudha0077/assessment_mobpro/ui/screen/MainScreen.kt
+++ app/src/main/java/com/satyayudha0077/assessment_mobpro/ui/screen/MainScreen.kt
@@ -2,17 +2,25 @@ package com.satyayudha0077.assessment_mobpro.ui.screen
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -25,9 +33,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.satyayudha0077.assessment_mobpro.R
@@ -67,25 +77,44 @@ fun MainScreen() {
 }
 
 @Composable
-fun ScreenContent(buah: Buah, modifier: Modifier = Modifier, onClick:() -> Unit) {
+fun ScreenContent(
+    buah: Buah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit
+) {
+
     var jumlah by remember { mutableStateOf("") }
 
+    var berat by remember { mutableStateOf("") }
+
+    val radioOptions = listOf(
+        stringResource(R.string.satuan),
+        stringResource(R.string.kilogram)
+    )
+    var jenis by remember {
+        mutableStateOf(radioOptions[0])
+    }
     Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = painterResource(id = buah.imageResId),
-            contentDescription = stringResource(R.string.gambar, buah.nama),
+            contentDescription = stringResource(
+                R.string.gambar,
+                buah.nama
+            ),
             contentScale = ContentScale.Crop,
-            modifier = Modifier.size(132.dp),
-
+            modifier = Modifier.size(200.dp)
         )
         Text(
             text = buah.nama,
             style = MaterialTheme.typography.headlineLarge,
-            modifier = Modifier.padding(top = 16.dp),
+            modifier = Modifier.padding(top = 12.dp)
         )
         OutlinedTextField(
             value = jumlah,
@@ -97,27 +126,97 @@ fun ScreenContent(buah: Buah, modifier: Modifier = Modifier, onClick:() -> Unit)
             },
             modifier = Modifier
                 .fillMaxWidth(0.9f)
-                .padding(top = 24.dp)
+                .padding(top = 12.dp)
         )
         OutlinedTextField(
-            value = jumlah,
+            value = berat,
             onValueChange = {
-                jumlah = it
+                berat = it
             },
             label = {
-                Text(text = stringResource(R.string.kilogram))
+                Text(text = stringResource(R.string.berat))
             },
             modifier = Modifier
                 .fillMaxWidth(0.9f)
-                .padding(top = 24.dp)
+                .padding(top = 12.dp)
         )
-        Button(
-            onClick = { onClick() },
-            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 24.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            Text(text = stringResource(R.string.lanjut))
+        Row (
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ){
+            radioOptions.forEach { text ->
+                FruitOption(
+                    label = text,
+                    isSelected = jenis == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = jenis == text,
+                            onClick = {
+                                jenis = text
+                            },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
         }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(top = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            Button(
+                onClick = {
+                    jumlah = ""
+                    berat = ""
+                    jenis = radioOptions[0]
+                },
+                modifier = Modifier.weight(1f),
+                shape = RoundedCornerShape(50.dp),
+                contentPadding = PaddingValues(
+                    vertical = 14.dp
+                )
+            ) {
+                Text(text = stringResource(R.string.ganti))
+            }
+            Button(
+                onClick = {
+                    onClick()
+                },
+                modifier = Modifier.weight(1f),
+                shape = RoundedCornerShape(50.dp),
+                contentPadding = PaddingValues(
+                    vertical = 14.dp
+                )
+            ) {
+                Text(text = stringResource(R.string.count))
+            }
+        }
+    }
+}
+
+@Composable
+fun FruitOption(
+    label: String,
+    isSelected: Boolean,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(
+            selected = isSelected,
+            onClick = null
+        )
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyMedium,
+            modifier = Modifier.padding(start = 4.dp)
+        )
     }
 }
 
